92e5669afb960d2efce86a04088fa0d34a665b5c
Boris <boris.thibaut.boulanger@gmail.com>
2019-05-09 11:48:03 +0200

    refactor calculate

--- src/main/java/fr/ReversePolishNotation.java
+++ src/main/java/fr/ReversePolishNotation.java
@@ -1,13 +1,7 @@
 package fr;
 
-public class ReversePolishNotation {
-    public static String calculate(String instructions) {
-        if (instructions.contains("2")) {
-            return "2";
-        }
-        if (instructions.contains("3")) {
-            return "3";
-        }
-        return "1";
+class ReversePolishNotation {
+    static String calculate(String instructions) {
+        return String.valueOf(instructions.charAt(0));
     }
 }
