95a425ab0d844adeddcfc536669ff1a8da894518
Boris <boris.thibaut.boulanger@gmail.com>
2019-05-09 10:24:02 +0200

    init

---
+++ src/test/java/fr/ReversePolishNotationTest.java
@@ -0,0 +1,12 @@
+package fr;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class ReversePolishNotationTest {
+    @Test
+    public void true_is_true() {
+        Assertions.assertThat(true).isTrue();
+    }
+}
