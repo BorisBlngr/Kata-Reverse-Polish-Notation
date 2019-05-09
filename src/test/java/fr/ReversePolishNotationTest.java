58b80426de73216ca0763d7dda7bfbc9efe61d07
Boris <boris.thibaut.boulanger@gmail.com>
2019-05-09 10:59:25 +0200

    zero plus two case test

--- src/test/java/fr/ReversePolishNotationTest.java
+++ src/test/java/fr/ReversePolishNotationTest.java
@@ -16,4 +16,10 @@ public class ReversePolishNotationTest {
         String result = ReversePolishNotation.calculate("0 1 +");
         assertThat(result).isEqualTo("1");
     }
+
+    @Test
+    public void zero_plus_two_return_two() {
+        String result = ReversePolishNotation.calculate("2 0 +");
+        assertThat(result).isEqualTo("2");
+    }
 }
